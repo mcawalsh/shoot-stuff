e32e5567bbfe7221810ca4fb7fa3b4531039c121
Mick Walsh <mca.walsh@gmail.com>
2019-01-26 14:24:41 +0000

    Spawn multiple small floors using manager

---
+++ Shoot-Stuff/Assets/Scripts/GameManagement/GameStateManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour
+{
+	private MapGenerator mapGenerator;
+	private GameObject[] floors;
+
+	[SerializeField]
+	public GameObject floorPrefab;
+	[SerializeField]
+	public int rooms = 2;
+
+    void Start()
+    {
+		Debug.Log("GameStateManager Starting...");
+
+		if (mapGenerator == null)
+			mapGenerator = new MapGenerator();
+
+		var positions = mapGenerator.GenerateMap(rooms);
+
+		BuildMap(positions);
+		
+    }
+
+	private void BuildMap(Vector3[] positions)
+	{
+		floors = new GameObject[rooms];
+
+		for (int i = 0; i < positions.Length; i++)
+		{
+			var pos = positions[i];
+
+			var newFloor = Instantiate(floorPrefab, pos, Quaternion.identity);
+			floors[i] = newFloor;
+		}
+	}
+
+    
+}
